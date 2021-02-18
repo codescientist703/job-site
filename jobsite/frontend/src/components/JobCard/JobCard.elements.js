1a7b9f526c45a140e382dbe1f0d2da262594a9f5
codescientist703 <pukimonlegend@gmail.com>
2021-02-18 19:38:05 +0530

    changes to card component

--- jobsite/frontend/src/components/JobCard/JobCard.elements.js
+++ jobsite/frontend/src/components/JobCard/JobCard.elements.js
@@ -2,80 +2,64 @@ import styled from 'styled-components/macro';
 import { FiStar } from 'react-icons/fi';
 
 export const Card = styled.div`
-
-    /* for mobile */
-    width: 90%;
-    box-shadow: 0 2px 7px rgba(0,0,0,0.2);
-    margin: 0 auto;
-    padding: 0.7rem 1.2rem;
-    border-radius: 10px;
-    margin-bottom: 1.2rem;
-    /* margin-top: 0.7rem; */
+	/* for mobile */
+	width: 90%;
+	box-shadow: 0 2px 7px rgba(0, 0, 0, 0.2);
+	margin: 0 auto;
+	padding: 0.7rem 1.2rem;
+	border-radius: 10px;
+	margin-bottom: 1.2rem;
+	/* margin-top: 0.7rem; */
 `;
 
 export const CardTop = styled.div`
-
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 1.5rem;
-
+	display: flex;
+	justify-content: space-between;
+	margin-bottom: 1.5rem;
 `;
 
 export const CardMiddle = styled.div`
-
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 1.5rem;
+	display: flex;
+	justify-content: space-between;
+	margin-bottom: 1.5rem;
 `;
 
 export const CardBottom = styled.div`
-
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-start;
+	display: flex;
+	justify-content: space-between;
+	align-items: flex-start;
 `;
 
-export const JobDetails = styled.div`
+export const JobDetails = styled.div``;
 
-    
-`;
-
-export const JobTitle = styled.p`
-
-
-`;
+export const JobTitle = styled.p``;
 
 export const JobCompany = styled.h3`
-
-    font-size: 1rem;
-    letter-spacing: 1px;
+	font-size: 1rem;
+	letter-spacing: 1px;
 `;
 
 export const StarIcon = styled(FiStar)`
-
-    font-size: 1.2rem;
+	font-size: 1.2rem;
 `;
 
-export const Item = styled.div`
-
-`;
+export const Item = styled.div``;
 
 export const ItemTitle = styled.h3`
-
-    font-size: 0.9rem;
+	font-size: 0.8rem;
 `;
 
 export const ItemDescription = styled.p`
-    font-size: 0.9rem;
+	font-size: 0.8rem;
 `;
 
 export const JobDescription = styled.p`
-    font-size: 0.85rem;
-    width: 60%;
+	font-size: 0.85rem;
+	width: 60%;
 `;
 
 export const Btn = styled.button`
-    border: none;
-    outline: none;
-    background-color: transparent;
+	border: none;
+	outline: none;
+	background-color: transparent;
 `;
--- jobsite/frontend/src/components/JobCard/JobCard.js
+++ jobsite/frontend/src/components/JobCard/JobCard.js
@@ -11,12 +11,13 @@ import {
 	Item,
 	ItemTitle,
 	ItemDescription,
-	Btn
+	Btn,
 } from './JobCard.elements';
-
+import { AiFillHome } from 'react-icons/ai';
+import { BiTimeFive } from 'react-icons/bi';
 const JobCard = () => {
-
-	const str = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi deleniti quaerat molestiae repellat facilis voluptatum vitae eligendi quod. Quos aperiam saepe asperiores tenetur amet sint itaque nostrum sunt distinctio fugit.';
+	const str =
+		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi deleniti quaerat molestiae repellat facilis voluptatum vitae eligendi quod. Quos aperiam saepe asperiores tenetur amet sint itaque nostrum sunt distinctio fugit.';
 
 	return (
 		<Card>
@@ -31,51 +32,42 @@ const JobCard = () => {
 			<CardMiddle>
 				<Item>
 					<ItemTitle>
+						<AiFillHome />
 						Location
 					</ItemTitle>
-					<ItemDescription>
-						Mumbai
-					</ItemDescription>
+					<ItemDescription>Mumbai</ItemDescription>
 				</Item>
 
 				<Item>
 					<ItemTitle>
+						<BiTimeFive />
 						Date
 					</ItemTitle>
-					<ItemDescription>
-						20-11-2021
-					</ItemDescription>
+					<ItemDescription>20-11-2021</ItemDescription>
 				</Item>
 
 				<Item>
 					<ItemTitle>
+						<BiTimeFive />
 						Salary
 					</ItemTitle>
-					<ItemDescription>
-						20,000
-					</ItemDescription>
+					<ItemDescription>20,000</ItemDescription>
 				</Item>
 
 				<Item>
 					<ItemTitle>
+						<BiTimeFive />
 						Experience
 					</ItemTitle>
-					<ItemDescription>
-						3+
-					</ItemDescription>
+					<ItemDescription>3+</ItemDescription>
 				</Item>
 			</CardMiddle>
 
 			<CardBottom>
 				<JobDescription>
-					{
-						str.length > 100 ? `${str.slice(0, 50)}...` : str
-					}
-
+					{str.length > 100 ? `${str.slice(0, 50)}...` : str}
 				</JobDescription>
-				<Btn>
-					View More &gt;
-				</Btn>
+				<Btn>View More &gt;</Btn>
 			</CardBottom>
 		</Card>
 	);
