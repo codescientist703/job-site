7f638fa5b6cb423a61e6f188d5dbfa43448419dc
NishidhJain <nishidhjain909@gmail.com>
2021-02-20 18:31:54 +0530

    Single Job component added

--- jobsite/frontend/src/pages/JobSingle/JobSingle.elements.js
+++ jobsite/frontend/src/pages/JobSingle/JobSingle.elements.js
@@ -1,1 +1,70 @@
 import styled from 'styled-components/macro';
+import { FiShare2 } from 'react-icons/fi';
+
+
+export const JobTitle = styled.h2`
+    text-align: center;
+    margin: 2rem 0;
+`;
+
+export const SingleJobContainer = styled.div`
+position: relative;
+    margin: 0 auto;
+    max-width: 1000px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    margin-bottom: 2rem;
+`;
+
+export const ShareIcon = styled(FiShare2)`
+
+    position: absolute;
+    right: 1rem;
+    margin-top: -1.8rem;
+`;
+
+export const Line = styled.div`
+
+    width: 97%;
+    height: 2px;
+    background-color: #ddd;
+    margin: 0 auto;
+`;
+
+export const JobDetails = styled.section`
+    padding: 0.7rem;
+
+	@media screen and (min-width: ${({ theme }) => theme.md}px) {
+		padding: 0.7rem 1.2rem;
+	}
+`;
+
+export const About = styled.h4`
+
+    font-size: 1.1rem;
+    margin-bottom: 1rem;
+`;
+
+export const Description = styled.p`
+
+`;
+
+export const ApplyBtn = styled.button`
+
+    color: #fff;
+    font-weight: 600;
+    font-size: 1.1rem;
+    padding: 0.6rem 1.3rem;
+    background-color: blue;
+    border: none;
+    outline: none;
+    border-radius: 5px;
+    margin: 1rem 0;
+`;
+
+
+
+
+
+
+
--- jobsite/frontend/src/pages/JobSingle/JobSingle.js
+++ jobsite/frontend/src/pages/JobSingle/JobSingle.js
@@ -1,4 +1,13 @@
-import {} from './JobSingle.elements';
+import {
+	JobTitle,
+	SingleJobContainer,
+	Line,
+	ShareIcon,
+	JobDetails,
+	About,
+	Description,
+	ApplyBtn
+} from './JobSingle.elements';
 import { JobCard, Container } from '../../components';
 
 import React from 'react';
@@ -6,7 +15,25 @@ import React from 'react';
 const JobSingle = () => {
 	return (
 		<Container>
-			<JobCard type={'single'} />
+			<JobTitle>
+				Web Developer at Flipkart
+			</JobTitle>
+
+			<SingleJobContainer>
+
+				<JobCard type={'single'} />
+				<ShareIcon />
+				<Line />
+				<JobDetails>
+					<About>About</About>
+					<Description>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, voluptates dolores. Harum illo cumque nisi provident, molestiae perspiciatis quaerat aliquam maxime, facilis quidem at minima quae consequatur et ipsam veritatis!
+					</Description>
+					<ApplyBtn>Apply Now</ApplyBtn>
+				</JobDetails>
+
+			</SingleJobContainer>
+
 		</Container>
 	);
 };
