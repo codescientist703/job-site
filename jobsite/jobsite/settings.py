3b0c3cd1b523c7147fb73ec1d7775b4d400d88df
codescientist703 <pukimonlegend@gmail.com>
2021-03-10 22:11:27 +0530

    debug false test

--- jobsite/jobsite/settings.py
+++ jobsite/jobsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ktr^k^n$f-6p4n@pvf7+hwzx93sgtg3=j%^_v&w$zt_c5$h3g)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['159.89.167.226']
 
