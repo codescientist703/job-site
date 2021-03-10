f91cbe7a31e0b5d019fad23d1bb7422ffbbf4e81
codescientist703 <pukimonlegend@gmail.com>
2021-03-10 22:01:55 +0530

    Added ip address

--- jobsite/jobsite/settings.py
+++ jobsite/jobsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ktr^k^n$f-6p4n@pvf7+hwzx93sgtg3=j%^_v&w$zt_c5$h3g)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['159.89.167.226']
 
 
 # Application definition
