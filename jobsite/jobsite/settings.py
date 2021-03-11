999adad5388027a10d6eca88b2f34de7037fc2cc
codescientist703 <pukimonlegend@gmail.com>
2021-03-11 16:24:29 +0530

    added security

--- jobsite/jobsite/settings.py
+++ jobsite/jobsite/settings.py
@@ -177,4 +177,11 @@ MATERIAL_ADMIN_SITE = {
     'SHOW_COUNTS': True,  # Show instances counts for each model
 
 }
+
+if DEBUG == False:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+
+
 CORS_ALLOW_ALL_ORIGINS = True
