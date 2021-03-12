bb875a8d26840472d4d4d3d5179279897b16412c
codescientist703 <pukimonlegend@gmail.com>
2021-03-12 16:59:32 +0530

    import export changes

--- jobsite/frontend/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Home } from './Home/Home';
-export { default as JobList } from './JobList/JobList';
-export { default as JobSingle } from './JobSingle/JobSingle';
-export { default as JobExperience } from './JobExperience/JobExperience';
-export { default as JobExperienceSingle } from './JobExperienceSingle/JobExperienceSingle';
-export { default as JobExperienceForm } from './JobExperienceForm/JobExperienceForm';
-export { default as Page404 } from './Page404/Page404';
