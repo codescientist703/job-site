ab8090152a3d114604d0b73705dbe0bee53e896c
codescientist703 <pukimonlegend@gmail.com>
2021-03-12 18:20:35 +0530

    added csrf token

--- jobsite/frontend/src/pages/JobExperienceForm/JobExperienceForm.js
+++ jobsite/frontend/src/pages/JobExperienceForm/JobExperienceForm.js
@@ -12,6 +12,21 @@ import { toast } from 'react-toastify';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 
+function getCookie(name) {
+	let cookieValue = null;
+	if (document.cookie && document.cookie !== '') {
+		const cookies = document.cookie.split(';');
+		for (let i = 0; i < cookies.length; i++) {
+			const cookie = cookies[i].trim();
+			// Does this cookie string begin with the name we want?
+			if (cookie.substring(0, name.length + 1) === name + '=') {
+				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+				break;
+			}
+		}
+	}
+	return cookieValue;
+}
 const SunEditor = React.lazy(() => import('suneditor-react'));
 const JobExperienceForm = () => {
 	const breadData = [
@@ -33,7 +48,11 @@ const JobExperienceForm = () => {
 				...formData,
 				content: content,
 			};
-			const response = await axios.post(apiUrl, validatedData);
+			const response = await axios.post(apiUrl, validatedData, {
+				headers: {
+					'X-CSRFToken': getCookie('csrftoken'),
+				},
+			});
 			successToast('Your experience has been successfully submitted !');
 		} catch (error) {
 			const errorData = error.response.data;
